0223770f6218bdcaf1e6887701ceb867a6e9e5a1
Noah Wooten <noah.wooten@icloud.com>
2022-02-02 10:32:17 -0500

    Initial Commit

---
+++ plang/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  plang
+//
+//  Created by Noah Wooten on 2/2/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
